053436a2ca1e34709ac99250d7a38e146b9dbd24
vladislav <v.kichkinev@microline.ru>
2024-12-17 18:10:27 +0300

    fix: validate phone

--- app/utils/validate_form_value.py
+++ app/utils/validate_form_value.py
@@ -3,7 +3,7 @@ import re
 
 def validate_value(value: str) -> str:
     EMAIL_REGEX = re.compile(r"^[\w\.-]+@[\w\.-]+\.\w+$")
-    PHONE_REGEX = re.compile(r"^\+7\s\d{3}\s\d{3}\s\d{2}\s\d{2}$")
+    PHONE_REGEX = re.compile(r"^\+7\s?\d{3}\s?\d{3}\s?\d{2}\s?\d{2}$")
     DATE_REGEX_DMY = re.compile(r"^\d{2}\.\d{2}\.\d{4}$")
     DATE_REGEX_YMD = re.compile(r"^\d{4}-\d{2}-\d{2}$")
 
